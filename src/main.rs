69893688a2493a29b142d652eda8c6f44732af60
ryota.sakamoto <ryota.sakamoto@aijus.com>
2018-03-14 00:30:28 +0900

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate log;
+extern crate simplelog;
 extern crate hyper;
 extern crate futures;
 
@@ -5,17 +7,23 @@ use std::fs::File;
 use std::path::Path;
 use std::env;
 use std::io::Read;
+use simplelog::*;
 use hyper::header::{ContentLength, ContentType};
 use hyper::server::{Http, Response, Request, Service};
 
 fn main() {
+    CombinedLogger::init(vec![
+        SimpleLogger::new(LevelFilter::Info, Config::default())
+    ]).unwrap();
     let mut args = env::args();
     let port = match args.len() {
         2 => args.nth(1).unwrap().parse().unwrap(),
         _ => 3000,
     };
 
-    let addr = format!("127.0.0.1:{}", port).parse().unwrap();
+    let ip = format!("127.0.0.1:{}", port);
+    info!("Start {}", ip);
+    let addr = ip.parse().unwrap();
     let server = Http::new().bind(&addr, || Ok(Server)).unwrap();
     server.run().unwrap();
 }
@@ -41,8 +49,9 @@ impl Service for Server {
 
 fn callback(req: &Request) -> Result<String, String> {
     let path = req.uri().path().to_string().replacen("/", "", 1);
+    info!("{}", path);
     let p = Path::new(&path);
-
+    
     let mut file = try!(File::open(&p).map_err(|e| e.to_string()));
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
