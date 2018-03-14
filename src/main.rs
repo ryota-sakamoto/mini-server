3d481ab19780675dc00ce408a5a6a0ccf3b4a178
ryota.sakamoto <ryota.sakamoto@aijus.com>
2018-03-15 01:50:59 +0900

    add use clap

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,27 @@
 extern crate simplelog;
 extern crate hyper;
 extern crate futures;
+extern crate clap;
 
 use std::fs::File;
 use std::path::Path;
-use std::env;
 use std::io::Read;
 use simplelog::*;
 use hyper::header::{ContentLength, ContentType};
 use hyper::server::{Http, Response, Request, Service};
+use clap::{Arg, App};
 
+/*
+# TODO
+- Add setting root path
+*/
 fn main() {
-    CombinedLogger::init(vec![
-        SimpleLogger::new(LevelFilter::Info, Config::default())
-    ]).unwrap();
-    let mut args = env::args();
-    let port = match args.len() {
-        2 => args.nth(1).unwrap().parse().unwrap(),
-        _ => 3000,
+    init_log();
+    let app = init_clap();
+    let matches = app.get_matches();
+    let port = match matches.value_of("port") {
+        Some(s) => s.parse().unwrap(),
+        None => 3000,
     };
 
     let ip = format!("127.0.0.1:{}", port);
@@ -26,6 +30,28 @@ fn main() {
     let addr = ip.parse().unwrap();
     let server = Http::new().bind(&addr, || Ok(Server)).unwrap();
     server.run().unwrap();
+}
+
+fn init_log() {
+     CombinedLogger::init(vec![
+        SimpleLogger::new(LevelFilter::Info, Config::default())
+    ]).unwrap();
+}
+
+fn init_clap<'a, 'b>() -> App<'a, 'b> {
+    App::new("mini-server")
+        .arg(Arg::with_name("port")
+            .help("port")
+            .short("p")
+            .long("port")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("root")
+            .help("document root")
+            .short("r")
+            .long("root")
+            .takes_value(true)
+        )
 }
 
 struct Server;
@@ -42,7 +68,10 @@ impl Service for Server {
             Err(e) => e,
         };
         Box::new(futures::future::ok(
-            Response::new().with_body(r)
+            Response::new()
+                .with_header(ContentLength(r.len() as u64))
+                .with_header(ContentType::plaintext())
+                .with_body(r)
         ))
     }
 }
